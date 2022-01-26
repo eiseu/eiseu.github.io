229b22bf4f4b2e8fc0a4ada4a11e1747785c221b
eiseu <54742918+eiseu@users.noreply.github.com>
2022-01-25 19:26:44 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider, responsiveFontSizes } from "@mui/material/styles";
+import { CssBaseline, styled } from "@mui/material";
+import Vods from "./vods/vods";
+import YoutubeVod from "./vods/YoutubeVod";
+import Navbar from "./navbar";
+import NotFound from "./utils/NotFound";
+import CustomVod from "./vods/CustomVod";
+
+export default function App() {
+  let darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      background: {
+        default: "#0e0e10",
+      },
+      primary: {
+        main: "#fff",
+      },
+      secondary: {
+        main: "#b39ddb",
+      },
+    },
+    components: {
+      MuiDrawer: {
+        styleOverrides: {
+          paper: {
+            color: "white",
+            backgroundImage: "none",
+          },
+        },
+      },
+    },
+  });
+
+  darkTheme = responsiveFontSizes(darkTheme);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline enableColorScheme />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <Parent>
+                <NotFound />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/"
+            element={
+              <Parent>
+                <Navbar />
+                <Vods />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/youtube/:vodId"
+            element={
+              <Parent>
+                <YoutubeVod type="live" />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/manual/:vodId"
+            element={
+              <Parent>
+                <CustomVod type="manual" />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/cdn/:vodId"
+            element={
+              <Parent>
+                <CustomVod type="cdn" />
+              </Parent>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+const Parent = styled((props) => <div {...props} />)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  min-height: 0;
+`;
